4325e32d5ea0ede6694a1f0b774078c858267fd4
mikael <mikael.fangel@pm.me>
2022-01-23 11:04:29 +0100

    added dynamic reading for wireless interfaces

--- setup.sh
+++ setup.sh
@@ -12,9 +12,12 @@ curl 'https://itswiki.compute.dtu.dk/images/0/07/Eduroam_aug2020.pem' -o $FOLDER
 read -r -p "Username: " USERNAME
 read -r -p "Password: " -s PASSWORD
 
+# Gets the name of the wireless interface using nmcli
+interface=$(nmcli dev status | grep -E "(^| )wifi( |$)" | awk '{print $1}')
+
 # Creates connection profile
 nmcli connection add \
- type wifi con-name "DTUsecure" ifname wlo1 ssid "DTUsecure" -- \
+ type wifi con-name "DTUsecure" ifname $interface ssid "DTUsecure" -- \
  wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 \
  802-1x.identity $USERNAME 802-1x.password $PASSWORD \
  802-1x.anonymous-identity "anonymous@dtu.dk" \
