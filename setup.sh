4864f574ee03e479ffd89f446cfe4fbb8f54d516
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-01-23 11:06:47 +0100

    changede variables to follow conventing for bash variables

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-FOLDER="/home/${USER}/.ca-cert"
+folder="/home/${USER}/.ca-cert"
 
 # Checks if folder is already made
-if [ ! -d "$FOLDER" ]; then
-  mkdir -p $FOLDER
+if [ ! -d "$folder" ]; then
+  mkdir -p $folder
 fi
 
-curl 'https://itswiki.compute.dtu.dk/images/0/07/Eduroam_aug2020.pem' -o $FOLDER/dtusecure.pem
+curl 'https://itswiki.compute.dtu.dk/images/0/07/Eduroam_aug2020.pem' -o $folder/dtusecure.pem
 
-read -r -p "Username: " USERNAME
-read -r -p "Password: " -s PASSWORD
+read -r -p "Username: " username
+read -r -p "Password: " -s password
 
 # Gets the name of the wireless interface using nmcli
 interface=$(nmcli dev status | grep -E "(^| )wifi( |$)" | awk '{print $1}')
@@ -19,6 +19,6 @@ interface=$(nmcli dev status | grep -E "(^| )wifi( |$)" | awk '{print $1}')
 nmcli connection add \
  type wifi con-name "DTUsecure" ifname $interface ssid "DTUsecure" -- \
  wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 \
- 802-1x.identity $USERNAME 802-1x.password $PASSWORD \
+ 802-1x.identity $username 802-1x.password $password \
  802-1x.anonymous-identity "anonymous@dtu.dk" \
- 802-1x.ca-cert $FOLDER/dtusecure.pem
+ 802-1x.ca-cert $folder/dtusecure.pem
