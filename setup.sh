640685f2f356f159fd17a2cfcdb6fd8c05ed01b1
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-02-03 09:04:57 +0100

    remove the use of the certificate

--- setup.sh
+++ setup.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-folder="/home/${USER}/.ca-cert"
-
-# Checks if folder is already made
-if [ ! -d "$folder" ]; then
-    echo "Creating folder ~/.ca-cert"
-    mkdir -p $folder
-fi
-
-cert="$folder/dtusecure.pem"
-
-# Checks if certificate is downloaded already
-if [ -f "$cert" ]; then
-    read -p "Certifacte already exists. Do you wish to redownload? [y/N] " answer
-
-    if [[ $answer == "y" || $answer == "Y" ]]; then
-        echo "Downloading certificate: "
-        curl 'https://itswiki.compute.dtu.dk/images/0/07/Eduroam_aug2020.pem' -o $folder/dtusecure.pem
-    else
-        echo "Skipping download of certificate..."
-    fi
-fi
-
 # Checks if the connection profile already exists
 if [[ $(nmcli -f GENERAL.STATE con show DTUsecure; echo $? == 0) ]]; then
     read -p "Connection profile already exists. \
@@ -48,5 +26,4 @@ nmcli connection add \
     type wifi con-name "DTUsecure" ifname $interface ssid "DTUsecure" -- \
     wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 \
     802-1x.identity $username 802-1x.password $password \
-    802-1x.anonymous-identity "anonymous@dtu.dk" \
-    802-1x.ca-cert $cert
+    802-1x.anonymous-identity "anonymous@dtu.dk"
