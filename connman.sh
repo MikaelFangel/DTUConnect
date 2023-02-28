277ac984b67929926431cfb9bfe80037d2bd0ac3
mikael <34864484+MikaelFangel@users.noreply.github.com>
2023-03-01 00:41:34 +0100

    linked script to setup and added errorhandling

--- connman.sh
+++ connman.sh
@@ -2,6 +2,17 @@
 # Source for configuration layout:
 # https://web.archive.org/web/20141213065729/https://software.intel.com/en-us/blogs/2014/12/04/connecting-intelr-edison-to-an-ieee-8021x-enterprise-hotspot-via-connman
 
+# Check if nmcli is installed before running the script
+if ! command -v connmanctl &> /dev/null; then
+    echo "connmanctl is not installed. Exiting script..."
+    exit 0
+fi
+
+if [ "$EUID" -ne 0 ];then 
+    echo $'\e[1;31m'Please run connman configuration as root$'\e[0m'
+  exit 1
+fi
+
 # Get credentials
 read -r -p "Username: " username
 read -r -p "Password: " password
@@ -28,3 +39,4 @@ connmanctl enable wifi
 connmanctl scan wifi
 service=$(connmanctl services | grep DTUsecure | tr -s ' ' | sed 's/^ //' | cut -d" " -f 2)
 connmanctl connect "$service"
+exit $?
